4fef231bbab5ce9dd571be326b340097b755fa22
Lorlothy <fael.rgt@gmail.com>
2023-06-14 18:04:06 -0300

    changes

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+"""
+Test for models
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelsTest(TestCase):
+    """Test models."""
+
+    def test_crete_user_wtih_email_sucessful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+    
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.com', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+    
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
