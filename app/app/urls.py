c1b996b0fe63f27efc79a9f8379522cb315b0df0
Lorlothy <fael.rgt@gmail.com>
2023-08-02 15:47:28 -0300

    Implement recipe listing API

--- app/app/urls.py
+++ app/app/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs',
     ),
-    path('api/user', include('user.urls')),
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 
 ]
