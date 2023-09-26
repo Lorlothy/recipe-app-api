15faf98037c329153ad512fea8685075e8a24bf7
Lorlothy <fael.rgt@gmail.com>
2023-09-26 19:13:49 -0300

    Configure Deploy

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zs9hqrxo9+ba4aw$94g+5czmi#!8*1j036_y6*-&y9h48sje(-'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
--- proxy/default.conf.tpl
+++ proxy/default.conf.tpl
@@ -1,13 +1,13 @@
 server {
     listen ${LISTEN_PORT};
 
-    local /static {
+    location /static {
         alias /vol/static;
     }
 
     location / {
-        uwsgi_pass          ${APP_HOST}:${APP_PORT};
-        include             /etc/nginx/uwsgi_params;
-        client_max_body_size 10M;
+        uwsgi_pass              ${APP_HOST}:${APP_PORT};
+        include                 /etc/nginx/uwsgi_params;
+        client_max_body_size    10M;
     }
 }
